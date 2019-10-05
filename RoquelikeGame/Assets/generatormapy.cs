9c658d978ab8f3ea0486fc0fdc472282f71d3762
DziQ96 <pawel.radzik321@gmail.com>
2019-10-05 15:38:34 +0200

    fixing collider

--- RoquelikeGame/Assets/generatormapy.cs
+++ RoquelikeGame/Assets/generatormapy.cs
@@ -6,10 +6,11 @@ public class generatormapy : MonoBehaviour
 {
     public int width;
     public int height;
-    public GameObject[] tablicaMap;
+    public List<GameObject> tablicaMap;
     // Start is called before the first frame update
     void Start()
     {
+        
         for(int i=0;i<height;i++)
             for (int j = 0; j < width; j++)
                 Instantiate(tablicaMap[Random.Range(0, 2)],new Vector3(62.5f*j,32.5f*i,0f), Quaternion.identity);
---
+++ RoquelikeGame/Assets/movment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour
+{
+    float x;
+    public float speed;
+    float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         y = Input.GetAxis("Vertical") * speed;
+         x = Input.GetAxis("Horizontal") * speed;
+
+        GetComponent<Rigidbody2D>().MovePosition(new Vector2(x+ transform.position.x, y+ transform.position.y));//= new Vector3(x + transform.position.x, y + transform.position.y, 0);
+    }
+
+}
