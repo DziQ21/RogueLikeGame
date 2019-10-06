ea40c6f2a5b432119e61c038fc14a45af9032b51
Bartosz Bień <bartekb340@gmail.com>
2019-10-06 23:45:38 +0200

    update wszystkiego
    119 plików... jak nic się nie wypieprzy to jutro kuuję kawę za 3zł

--- RoquelikeGame/Assets/generatormapy.cs
+++ RoquelikeGame/Assets/generatormapy.cs
@@ -7,13 +7,15 @@ public class generatormapy : MonoBehaviour
     public int width;
     public int height;
     public List<GameObject> tablicaMap;
+    
     // Start is called before the first frame update
     void Start()
     {
         
-        for(int i=0;i<height;i++)
+        for(int i=0;i<height;i++) 
             for (int j = 0; j < width; j++)
-                Instantiate(tablicaMap[Random.Range(0, 2)],new Vector3(62.5f*j,32.5f*i,0f), Quaternion.identity);
+                Instantiate(tablicaMap[Random.Range(0, 3)],new Vector3(62.5f*j,32.5f*i,0f), Quaternion.identity);
+      
     }
 
     // Update is called once per frame
