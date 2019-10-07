90e19c7dd8f89e07fb2b2ae4e8d4375cd23c11ef
DziQ21 <pawel.radzik321@gmail.com>
2019-10-07 19:21:32 +0200

    Finished Hp script

--- RoquelikeGame/Assets/HPMenager.cs
+++ RoquelikeGame/Assets/HPMenager.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class HPScript : ScriptableObject
+public class HPScript : MonoBehaviour
 {
     public int hP;
+    public UnityEvent deathevent;
     // Start is called before the first frame update
     public void ReciveDamage(int damage)
     {
+        hP -= damage;
+        if (hP <= 0)
+            Die();
 
+    }
+    void Die()
+    {
+        deathevent.Invoke();
+        Destroy(this.gameObject);
     }
 }
