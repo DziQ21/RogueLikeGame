c73ea7cc76f17e7c47e05cd86ba8c53dd72e3e18
DziQ21 <pawel.radzik321@gmail.com>
2019-10-09 18:01:10 +0200

    creating question menager

--- RoquelikeGame/Assets/Questnemager.cs
+++ RoquelikeGame/Assets/Questnemager.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
 public class Questnemager : MonoBehaviour
 {
-
-    void SetQuests(List<Questnemager> quets)
+    public static List<Quest> questList;
+    public static void AddQuests(List<Quest> quests)
     {
-        
+        questList.AddRange(quests);
     }
 
     
--- RoquelikeGame/Assets/generatormapy.cs
+++ RoquelikeGame/Assets/generatormapy.cs
@@ -11,11 +11,15 @@ public class generatormapy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        for(int i=0;i<height;i++) 
+
+        for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++)
-                Instantiate(tablicaMap[Random.Range(0, tablicaMap.Count)],new Vector3(62.5f*j,32.5f*i,0f), Quaternion.identity);
-      
+            {
+                int randomNumber = Random.Range(0, tablicaMap.Count);
+                Instantiate(tablicaMap[randomNumber], new Vector3(62.5f * j, 32.5f * i, 0f), Quaternion.identity);
+                Questnemager.AddQuests(tablicaMap[randomNumber].GetComponent<QuestList>().list);
+
+            }
     }
 
     // Update is called once per frame
