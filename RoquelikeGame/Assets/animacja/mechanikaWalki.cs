8e0a11f252268824b7037f67bd0699a6cd7d3ece
Bartosz Bień <bartekb340@gmail.com>
2019-10-11 22:14:06 +0200

    Update mechanikaWalki.cs

--- RoquelikeGame/Assets/animacja/mechanikaWalki.cs
+++ RoquelikeGame/Assets/animacja/mechanikaWalki.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class mechanikaWalki : MonoBehaviour
 {
 
-	public float wywolanie = 5.0f;
+	public float wywolanie = 2.5f;
 	public float licznikBartka = 0.0f;
-	private bool t = false;
+	private bool t = false;			 //ono chyba jest niepotrzebne
 	private float kat;
+	public GameObject gameWeapon;
  // Start is called before the first frame update
     void Start()
     {
@@ -19,26 +20,33 @@ public class mechanikaWalki : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	//if ((licznikBartka >= wywolanie) || !t) {
-	//	wywolanie = 5.0f;
-	//	licznikBartka = 0.0f;
-	//	
-	//}
-
+	if (licznikBartka >= wywolanie) {
+		gameWeapon.SetActive (true);
+	}
 
 	if ((licznikBartka < wywolanie) && t) {
        		transform.rotation = Quaternion.Euler(0,0,kat);
 		kat+=5;
 		licznikBartka += 0.1f;
+		gameWeapon.SetActive (true);
 	}
+
+	//if ((licznikBartka < wywolanie) && t && kat < 180) {
+       	//	transform.rotation = Quaternion.Euler(0,0,kat);
+	//	kat-=5;
+	//	licznikBartka += 0.1f;
+	//	gameWeapon.SetActive (true);
+	//}
+
 
     }
 
     internal void MakeAttack(float v)
    {
-       	kat = v;
+       	kat = v + 160;
 	t = true;
-	wywolanie = 5.0f;
+	wywolanie = 2.5f;
 	licznikBartka = 0.0f;
-    }
+	gameWeapon.SetActive (true);
+  }
 }
