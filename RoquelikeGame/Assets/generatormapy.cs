e793b53af6bc5b628e6a267ec1f70a230a4caf39
Bartosz Bień <bartekb340@gmail.com>
2019-10-13 22:37:51 +0200

    fixed bugs with map edges
    now it should work properly. I deleted old prefabs with edges and I created two new instead of them. What is more, I decided to change the code in script named "generatormapy.cs". I changed the numbers/borders from the puzzle. Then I added two if's, because of them some edges won't be created so many times. It should work... At the end I deleted old files

--- RoquelikeGame/Assets/generatormapy.cs
+++ RoquelikeGame/Assets/generatormapy.cs
@@ -18,20 +18,22 @@ public class generatormapy : MonoBehaviour
         for (int i = 0; i < height; i++)
         {   
         
-                Instantiate(kraniecPionowy, new Vector3(-82.5f, 32.5f * i, 0f), Quaternion.identity);
+                Instantiate(kraniecPionowy, new Vector3(-42f, 32.5f * i, 0f), Quaternion.identity);
 
             for (int j = 0; j < width; j++)
-            {
-                Instantiate(kraniecPoziomy, new Vector3(62.5f * j, (width-1) * 32.5f + 68f, 0f), Quaternion.identity);
+            {   
+		if (i == 0)
+               	 Instantiate(kraniecPoziomy, new Vector3(62.5f * j, (width-1) * 32.5f + 29.5f, 0f), Quaternion.identity);
 
                 int randomNumber = Random.Range(0, tablicaMap.Count);
                 Instantiate(tablicaMap[randomNumber], new Vector3(62.5f * j, 32.5f * i, 0f), Quaternion.identity);
                 Questnemager.AddQuests(tablicaMap[randomNumber].GetComponent<QuestList>().list);
                
-                Instantiate(kraniecPoziomy, new Vector3(62.5f * j, -66f, 0f), Quaternion.identity);
+		if (i == height - 1)
+               	 Instantiate(kraniecPoziomy, new Vector3(62.5f * j, -27f, 0f), Quaternion.identity);
 
             }
-            Instantiate(kraniecPionowy, new Vector3((width-1) * 62.5f + 73.5f, 32.5f * i, 0f), Quaternion.identity);
+            Instantiate(kraniecPionowy, new Vector3((width-1) * 62.5f + 44.5f, 32.5f * i, 0f), Quaternion.identity);
         }
     }
 
