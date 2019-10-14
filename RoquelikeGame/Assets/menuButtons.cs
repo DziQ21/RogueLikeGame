fba8278dfbeece2af80f52b1ac09fd43e0e80352
Bartosz Bień <bartekb340@gmail.com>
2019-10-14 20:29:48 +0200

    title menu added
    title menu added in a new scene with a temporary game title RIVERLAND

---
+++ RoquelikeGame/Assets/menuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButtons : MonoBehaviour
+{
+   public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void quitGame()
+    {
+        Debug.Log("Exited from the game");
+        Application.Quit();
+    }
+    public void openControls()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
